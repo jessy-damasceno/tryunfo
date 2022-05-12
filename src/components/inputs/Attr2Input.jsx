7fb5a3c2354559cf9ae6951216154f91106a4b34
Jessy Damasceno <jessy_bas@hotmail.com>
2022-05-12 17:35:49 -0300

    add Attr2Input props and validations

--- src/components/inputs/Attr2Input.jsx
+++ src/components/inputs/Attr2Input.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Attr2Input extends Component {
   render() {
+    const { value, onChange } = this.props;
+
     return (
       <div className="container">
         <label htmlFor="attr2-input">
@@ -11,9 +14,16 @@ export default class Attr2Input extends Component {
             data-testid="attr2-input"
             name="attr2-input"
             id="attr2-input"
+            value={ value }
+            onChange={ onChange }
           />
         </label>
       </div>
     );
   }
 }
+
+Attr2Input.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
