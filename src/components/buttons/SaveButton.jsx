4d68d0ee02646ca8eaa01a5a687ba9f0aba79a04
Jessy Damasceno <jessy_bas@hotmail.com>
2022-05-12 17:59:57 -0300

    add all props and validations

--- src/components/buttons/SaveButton.jsx
+++ src/components/buttons/SaveButton.jsx
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class SaveInput extends Component {
   render() {
+    const { disabled, onChange } = this.props;
+
     return (
       <button
         data-testid="save-button"
         type="submit"
         id="save-button"
+        disabled={ disabled }
+        onChange={ onChange }
       >
         Salvar
       </button>
     );
   }
 }
+
+SaveInput.propTypes = {
+  disabled: PropTypes.bool.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
